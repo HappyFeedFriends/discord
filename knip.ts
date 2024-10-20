d0b6914a8b64d2f7d3b610b8495511496382369b
HappyFeedFriends <ruslan776@list.ru>
2024-10-20 16:13:55 +0500

    Поправлены линтинги

--- knip.ts
+++ knip.ts
@@ -2,15 +2,16 @@ import type { KnipConfig } from 'knip';
 
 const knipCfg: KnipConfig = {
   entry: [
-    'src/index.ts!',
+    'src/{client,server,app}/{index,vite.config}.{ts,tsx}!',
   ],
   project: [
     'src/**/*.{js,ts,jsx,tsx}!',
     '!src/typings/**/*',
   ],
-  ignoreDependencies: ['jest-junit', 'webpack-cli', '^eslint-.*'],
+  ignoreDependencies: ['^eslint-.*'],
   rules: {
     unlisted: 'off',
+    binaries: 'off'
   },
 };
 
